9b6584fafc7c1536fb97059e20b6c0158b893844
Mihaly Ale <105678319+mihaly-ale@users.noreply.github.com>
2024-08-12 21:18:50 +0200

    Update nav menu button state toggle logic

--- nav.js
+++ nav.js
@@ -1,16 +1,32 @@
 const navMenuBtn = document.getElementById("nav-menu-button")
 const navbar = document.getElementById("nav")
+const bar = navMenuBtn.children[1]
 let isNavOpen = false
 
-navMenuBtn.addEventListener("click", slideInNavigation)
+navMenuBtn.addEventListener("click", () => {
+  slideInNavigation(); changeNavMenuBtn()
+})
 
 function slideInNavigation() {
-
   if (isNavOpen) {
     navbar.classList.add("nav-off-screen")
+    bar.classList.remove("change-menu-bar")
+    bar.textContent = "—"
     isNavOpen = false
   } else {
     navbar.classList.remove("nav-off-screen")
+    bar.classList.add("change-menu-bar")
+    bar.textContent = "✕"
     isNavOpen = true
+  }
+}
+
+function changeNavMenuBtn() {
+  if (!isNavOpen) {
+    bar.classList.remove("change-menu-bar")
+    bar.textContent = "—"
+  } else {
+    bar.classList.add("change-menu-bar")
+    bar.textContent = "✕"
   }
 }
