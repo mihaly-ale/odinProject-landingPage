87b63a9446b2e5ba1475d127ee73b4a03f5d2974
Mihaly Ale <105678319+mihaly-ale@users.noreply.github.com>
2024-08-12 20:58:05 +0200

    Refactor navbar visibility logic into CSS class

--- nav.js
+++ nav.js
@@ -1,10 +1,16 @@
 const navMenuBtn = document.getElementById("nav-menu-button")
+const navbar = document.getElementById("nav")
+let isNavOpen = false
 
 navMenuBtn.addEventListener("click", slideInNavigation)
 
-
 function slideInNavigation() {
-  const nav = document.getElementById("nav")
 
-  nav.style.translate = 0;
+  if (isNavOpen) {
+    navbar.classList.add("nav-off-screen")
+    isNavOpen = false
+  } else {
+    navbar.classList.remove("nav-off-screen")
+    isNavOpen = true
+  }
 }
